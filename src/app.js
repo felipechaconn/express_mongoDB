8b1127b69e18515d818fecc6c5310f15f2e8786f
Luis Chacón Navarro <felipechaconn@Felipe-MacBook-Pro.local>
2022-03-08 18:10:22 -0600

    Handlebars(Templeate Engine)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
 //This files content app configuration
 
-//Import express framework
+/**
+ * Import express framework
+ * Import express handle-bars to work with html
+ * Import Routes file
+ * Import Module path to work with locations in computer
+ *
+ */
 import express from "express";
-
-//Import Routes file
-import indexRoutes from './routes/index.routes'
+import { create } from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
 
 const app = express();
+
+//Configure views propery
+app.set("views", path.join(__dirname, "views"));
+/**
+ * Tell Express we are going to use motor handelbars
+ * layoutsDir= common html structure
+ * defaultLayout= default hbs file
+ * extname= files extension name.
+ *
+ */
+const exphbs = create({
+  extname: ".hbs",
+  layoutsDir: path.join("src/views", "layouts"),
+  defaultLayout: "main",
+});
+
+//create motor
+app.engine(".hbs", exphbs.engine);
+//set to express template motor
+app.set("view engine", ".hbs");
 
 //Need to use index routes
 app.use(indexRoutes);
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,14 +1,26 @@
 //This import is a function to avoid call app again.
-import {Router} from 'express'
+import { Router } from "express";
 
 //Execute Router
 const router = Router();
 
+//Route
 router.get("/", (req, res) => {
-    res.send("Hello Mundo");
-  });
+  //Render works with views
+  res.render("index");
+});
 
+//Route about us
+router.get("/about", (req, res) => {
+  //Render works without extension because app.js knows we work with .hbs
+  res.render("about");
+});
 
+//Route about us
+router.get("/edit", (req, res) => {
+  //Render works without extension because app.js knows we work with .hbs
+  res.render("edit");
+});
 
 //Export this routes
 export default router;
