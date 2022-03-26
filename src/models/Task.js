8230c5a86934b020e940c4380bf35be3747cb0fd
Luis Chacón Navarro <felipechaconn@Felipe-MacBook-Pro.local>
2022-03-26 11:43:47 -0600

    Mongoose models

---
+++ src/models/Task.js
@@ -0,0 +1,28 @@
+// Modelo de datos por eso en mayuscula
+
+import { Schema, model } from "mongoose";
+
+//Define schema
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true, //Clear string
+    },
+    description: String,
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, //Send CreatedAt UpdatedAt
+    versionKey: false, // Mongoose adds  default __v:  to our documents.
+  }
+);
+
+//Create data model
+
+export default model("Task", taskSchema);
